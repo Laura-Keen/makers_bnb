d2dcf1a2e16c6df20ba01f45b6e73e1acaeb5fe3
Rafael Swiderski <76166627+raf-swiderski@users.noreply.github.com>
2021-03-02 17:44:36 +0000

    adding line 7 'connection.exec("TRUNCATE listings;")'

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,5 +3,7 @@ require 'pg'
 def setup_test_database
 connection = PG.connect(dbname: 'makers_bnb_test')
 # Clear the bookmarks table
-connection.exec("TRUNCATE users;")
+connection.exec("TRUNCATE users;") 
+connection.exec("TRUNCATE listings;")
 end
+
