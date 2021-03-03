c9758b00f09de585952cb2412cdc2b82b456f03e
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-03-03 16:05:00 +0100

    fixed persisted data method

--- spec/unit/listings_spec.rb
+++ spec/unit/listings_spec.rb
@@ -1,14 +1,15 @@
 require 'listings'
 require 'pg'
+require 'database_helpers'
 
 describe Listings do
 
     describe '.create' do
         it 'allows a new listing to be added' do
             listings = Listings.create(username: 'bob', title: 'my house', description: 'it is tiny', price: '50.00')
-            #persisted_data = persisted_data(listing_id: listings.listing_id)
+            persisted_data = listings_persisted_data(listing_id: listings.listing_id)
             expect(listings).to be_a Listings
-            #expect(listings.listing_id).to eq persisted_data['listing_id']
+            expect(listings.listing_id).to eq listings_persisted_data['listing_id']
             expect(listings.username).to eq 'bob'
             expect(listings.title).to eq 'my house'
             expect(listings.description).to eq 'it is tiny'
