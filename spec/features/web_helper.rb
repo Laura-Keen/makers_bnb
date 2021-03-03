dcb294e3f35d1564acb795ff4b6095fb315745cc
Aniruddh <ojhaa94@gmail.com>
2021-03-03 15:37:18 +0000

    connects user table to app file

--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -3,8 +3,8 @@ def sign_up
 	click_link 'Sign Up'
 	fill_in :first_name, with: 'bob'
 	fill_in :last_name, with: 'smith'
-	fill_in :username, with: 'test user'
-	fill_in :email, with: 'testuser@example.com'
-	fill_in :password, with: 'password123'
+	fill_in :username, with: 'bobby'
+	fill_in :email, with: 'bob@smith.com'
+	fill_in :password, with: 'cabbage'
 	click_button 'Sign Up'
 end
